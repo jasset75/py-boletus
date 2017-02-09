5d019f4ee5f020260b30bbad524e17a4d733662b
aguilarJuanA34H [win] <jantonio.aguilar.sspa@juntadeandalucia.es>
2017-02-09 13:52:14 +0100

    test development

--- test/test_data.py
+++ test/test_data.py
@@ -7,8 +7,8 @@ def draw_to_str(draw,sep='_'):
   return sep.join([draw['N1'],draw['N2'],draw['N3'],draw['N4'],draw['N5'],draw['N6']])
 
 
-def check_draw(f_in,draw,f_out=None,experiment='results'):
-  df = pd.read_csv(f_in)
+def check_draw(df_lot,draw,f_out=None,experiment='results',sort=True):
+  df = df_lot.copy()
   df['draw'] = draw
   df['success'] = 0
   df['comp'] = False
@@ -41,13 +41,15 @@ if __name__ == '__main__':
   df_test = pd.read_csv(f_test,names=['N1','N2','N3','N4','N5','N6'])
   
   f_in = r'c:\work\github\py-boletus\data\ES-bonoloto.csv'
+  df_lot = pd.read_csv(f_in,parse_dates=['FECHA'])  
   f_out = 'c:\\work\\github\\py-boletus\\out\\{0}_{1}_results_{2}.csv'
   df_total = None
   for i, draw in df_test.iterrows():
-    df_parcial = check_draw(f_in,draw)
+    df_parcial = check_draw(df_lot,draw,sort=False)
     if df_total is not None:
       df_total = df_total.append(df_parcial)
     else:
       df_total = df_parcial.copy()
+  df_total.sort_values(by=['success','comp','FECHA'],ascending=False,inplace=True)
   max_num_success = df_total['success'].max()
   df_total.to_csv(f_out.format(max_num_success,'boletus','M'))
